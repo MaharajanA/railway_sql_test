593a14ea4414daf6761d0990a8f1cc254c083500
MaharajanA <117497208+MaharajanA@users.noreply.github.com>
2025-01-10 15:05:50 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import login_view, signup_view, dashboard_view
+from .views import login_view, signup_view, dashboard_view , index_view
 
 urlpatterns = [
-    path('', login_view, name='login'),
+    path('', index_view , name='index'),
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('dashboard/', dashboard_view, name='dashboard'),
